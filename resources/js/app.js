cb87fb179f96f041769db9a5121451c0ba0375d8
izarooni <coalasonly@gmail.com>
2021-04-09 18:08:57 -0600

    Fix gallery layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,16 @@
 require('./bootstrap');
 
+// lazy load images
+(function() {
+    let images = document.getElementsByTagName('img');
+    for (let i = 0; i < images.length; i++) {
+        // lazy load image
+        let image = images[i];
+        if (!image.attributes.lazysrc) continue;
+        image.src = image.attributes.lazysrc.value;
+    }
+})();
+
 // display a modal showing the full-sized image
 (function () {
     let e = document.getElementById("gallery-modal");
@@ -10,6 +21,7 @@ require('./bootstrap');
         const IMAGES = document.getElementsByClassName('gallery-img');
         for (let i = 0; i < IMAGES.length; i++) {
             let image = IMAGES[i];
+
             image.addEventListener('click', function () {
                 MODAL_IMAGE.src = image.src;
                 MODAL.show();
