8b562e32d0322962fe6112ed5c602ede9bc291c4
izarooni <coalasonly@gmail.com>
2021-04-21 10:33:21 -0600

    Fix UI for reservation viewing

---
+++ resources/views/public/old.reserve.blade.php
@@ -0,0 +1,147 @@
+<x-app>
+    @section('title', '- Reserve')
+
+    @section('head')
+        <!-- date picker -->
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+    @endsection
+
+    <div class="container-md container-sm-fluid mt-4">
+        <x-errors></x-errors>
+
+        <div class="row">
+            @env('local')
+            <div class="order-1 order-md-0 col-md-5 col-lg mt-sm-4">
+                <div id="map" class="h-100" style="min-height: 80vh;"></div>
+            </div>
+            @endenv
+            <div class="order-0 col">
+                <form name="reserve_form" method="POST" action="reserve">
+                    @csrf
+                    <!-- Customer Name -->
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="">Name</span>
+                        </div>
+                        <input type="text" class="form-control" placeholder="First Name" name="first_name" autofocus
+                            required value="{{ old('first_name') }}" />
+                        <input type="text" class="form-control" placeholder="Last Name" name="last_name" required
+                            value="{{ old('last_name') }}" />
+                    </div>
+
+                    <!-- Customer Email -->
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">Email</span>
+                        </div>
+                        <input type="email" class="form-control" placeholder="example@hotmail.com" name="email"
+                            required value="{{ old('email') }}">
+                    </div>
+
+                    <!-- Customer Phone -->
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">Phone</span>
+                        </div>
+                        <input type="tel" class="form-control" placeholder="123 456 7890" name="phone" required value="{{ old('phone') }}">
+                    </div>
+
+                    <!-- Customer Age -->
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">Age</span>
+                        </div>
+                        <input type="number" class="form-control" min="18" value="18" name="age" autocomplete="false"
+                            required>
+                    </div>
+
+                    <!-- Camping Type -->
+                    <fieldset class="form-group mb-3">
+                        <div class="row">
+                            <legend class="col-form-label col-sm-2 pt-0">Campground</legend>
+                            <div class="col-sm-10">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="camping_type" id="ctSingle"
+                                        value="0" checked>
+                                    <label class="form-check-label" for="ctSingle">Medium-sized Tent</label>
+                                    <small class="text-muted form-text">&HorizontalLine; $39 per night</small>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="camping_type" id="ctDouble"
+                                        value="1">
+                                    <label class="form-check-label" for="ctDouble">Extra Medium-sized Tent</label>
+                                    <small class="text-muted form-text">&HorizontalLine; $30 flat fee</small>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="camping_type" id="ctRV"
+                                        value="2">
+                                    <label class="form-check-label" for="ctRV">RV Spot</label>
+                                    <small class="text-muted form-text">&HorizontalLine; $69 per night</small>
+                                </div>
+                            </div>
+                        </div>
+                    </fieldset>
+
+                    <!-- Reservation dates -->
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">Reservation</span>
+                        </div>
+                        <input type="text" class="form-control" name="dates" required autocomplete="off">
+                        <small class="form-text text-muted w-100" id="day_count"></small>
+                    </div>
+
+                    <!-- Number of campers -->
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">Number of Campers</span>
+                        </div>
+                        <input type="number" class="form-control" name="campers" value="1" min="1" max="6"
+                            autocomplete="false">
+                        <small class="form-text text-muted w-100">The amount of people staying, including
+                            yourself.</small>
+                    </div>
+
+                    <div class="container" id="campers">
+                    </div>
+
+                    <h3>Total: <small class="text-muted" id="total">Calculating...</small></h3>
+
+                    <button type="submit" class="btn btn-primary">Pay now</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="maps-modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="properties" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modal-name"></h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <dl class="row">
+                        <dt class="col-sm-3">Description</dt>
+                        <dd class="col-sm-9" id="modal-description"></dd>
+                    </dl>
+                </div>
+            </div>
+        </div>
+    </div><!-- Modal end -->
+
+    @section('scripts')
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+        <script src="{{ asset('js/checkout.js') }}"></script>
+
+        <script
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRE6SEIjNbmklk--s8yVx3XbyRmzC3yNM&callback=initMap&v=weekly"
+            async>
+        </script>
+        <script src="{{ asset('js/map.js') }}"></script>
+    @endsection
+</x-app>
--- resources/views/public/reservation.blade.php
+++ resources/views/public/reservation.blade.php
@@ -1,101 +1,112 @@
 <x-app>
     <div class="container mt-4">
-        <div class="row">
-            <div class="col card mx-md-4">
+
+        <div class="row gap-3 justify-content-center">
+            <div class="col-xs-6 col-lg-5 card">
                 <div class="card-body">
                     <h3 class="title">Reservation</h3>
                     <hr />
-                    <p><span class="text-muted">Arrival</span> <?= date('Y-m-d (l F j)', strtotime($reservation->date_in)) ?></p>
-                    <p><span class="text-muted">Departure</span> <?= date('Y-m-d (l F j)', strtotime($reservation->date_out)) ?></p>
+                    <dl class="row">
+                        <dt class="col-sm-4">Arrival</dt>
+                        <dd class="col-sm-8"><?= date('m-d-Y (F j, Y)', strtotime($reservation->date_in)) ?></dd>
+
+                        <dt class="col-sm-4">Departure</dt>
+                        <dd class="col-sm-8"><?= date('m-d-Y (F j, Y)', strtotime($reservation->date_out)) ?></dd>
+                    </dl>
                 </div> <!-- card body end -->
             </div> <!-- card/column end -->
             
-            <div class="col card mx-4">
+            <div class="col-xs-6 col-lg-5 card">
                 <div class="card-body">
                     <h3 class="title">Customer</h3>
                     <hr />
-                    <p class="lead">{{ $reservation->first_name }} {{ $reservation->last_name }}</p>
+                    <dl class="row">
+                        <dt class="col-sm-3">Name</dt>
+                        <dd class="col-sm-9">{{ $reservation->first_name }} {{ $reservation->last_name }}</dd>
 
-                    <p>
-                        <span class="text-muted">Email</span>
-                        <a href="mailto:{{ $reservation->email }}">{{ $reservation->email }}</a>
-                    </p>
-
-                    <p>
-                        <span class="text-muted">Phone</span>
-                        <a href="tel:{{ $reservation->phone }}">{{ $reservation->phone }}</a>
-                    </p>
-
-                    <p><span class="text-muted">Age</span> {{ $reservation->age }}</p>
+                        <dt class="col-sm-3">Email</dt>
+                        <dd class="col-sm-9">
+                            <a href="mailto:{{ $reservation->email }}">{{ $reservation->email }}</a>
+                        </dd>
+                        
+                        <dt class="col-sm-3">Phone</dt>
+                        <dd class="col-sm-9">
+                            <a href="tel:{{ $reservation->phone }}">{{ $reservation->phone }}</a>
+                        </dd>
+                        
+                        <dt class="col-sm-3">Age</dt>
+                        <dd class="col-sm-9">{{ $reservation->age }}</dd>
+                    </dl>
                 </div> <!-- card body end -->
             </div> <!-- column/card end -->
         </div> <!-- row end -->
 
-        <div class="my-5">
-            <h3 class="title">Campers</h3>
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First Name</th>
-                        <th scope="col">Last Name</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @for ($i = 0; $i < sizeof($campers); $i++)
+        <div class="row gap-sm-5 gap-md-0 mt-5">
+            <div class="col-xs-12 col-md-6">
+                <h3 class="title">Campers</h3>
+                <table class="table table-hover">
+                    <thead>
                         <tr>
-                            <th scope="row">{{ $i + 1 }}</th>
-                            <td>{{ $campers[$i]->first_name }}</td>
-                            <td>{{ $campers[$i]->last_name }}</td>
+                            <th scope="col">#</th>
+                            <th scope="col">First Name</th>
+                            <th scope="col">Last Name</th>
                         </tr>
-                    @endfor
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        @for ($i = 0; $i < sizeof($campers); $i++)
+                            <tr>
+                                <th scope="row">{{ $i + 1 }}</th>
+                                <td>{{ $campers[$i]->first_name }}</td>
+                                <td>{{ $campers[$i]->last_name }}</td>
+                            </tr>
+                        @endfor
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="col-xs-12 col-md-6">
+                <h3 class="title">Pricing</h3>
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">Item</th>
+                            <th scope="col">Qty.</th>
+                            <th scope="col">Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td scope="row">Nights reserved</td>
+                            <td>{{ $nights }}</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            @if ($reservation->camping_type != 2)
+                                <td scope="row">Medium tent</td>
+                                <td>{{ $reservation->camping_type == 0 ? 1 : 2 }}</td>
+                                <td>$39</td>
+    @else
+                                <td scope="row">RV spot</td>
+                                <td>1</td>
+                                <td>$69</td>
+                            @endif
+                        </tr>
+                        @if ($reservation->camping_type == 1)
+                            <tr>
+                                <td scope="row">Extra medium tent</td>
+                                <td>1</td>
+                                <td>$30</td>
+                            </tr>
+                        @endif
+                        <tr>
+                            <td scope="row"><strong>Total</strong></td>
+                            <td></td>
+                            <td><strong>${{ $cost }}</strong></td>
+                        </tr>
+                    </tbody>
+                </table>    
+            </div>    
         </div>
 
-        <div class="my-5">
-            <h3 class="title">Pricing</h3>
-            <hr />
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col">Item</th>
-                        <th scope="col">Qty.</th>
-                        <th scope="col">Total</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td scope="row">Nights reserved</td>
-                        <td>{{ $nights }}</td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        @if ($reservation->camping_type != 2)
-                            <td scope="row">Medium tent</td>
-                            <td>{{ $reservation->camping_type == 0 ? 1 : 2 }}</td>
-                            <td>$39</td>
-                        @else
-                            <td scope="row">RV spot</td>
-                            <td>1</td>
-                            <td>$69</td>
-                        @endif
-                    </tr>
-                    @if ($reservation->camping_type == 1)
-                        <tr>
-                            <td scope="row">Extra medium tent</td>
-                            <td>1</td>
-                            <td>$30</td>
-                        </tr>
-                    @endif
-                    <tr>
-                        <td scope="row"><strong>Total</strong></td>
-                        <td></td>
-                        <td><strong>${{ $cost }}</strong></td>
-                    </tr>
-                </tbody>
-            </table>    
-        </div>    
     </div> <!-- container end -->
 </x-app>
-
