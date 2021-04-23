bf3a31ff278b0a82fdcc751734dfc377bf0307ad
izarooni <coalasonly@gmail.com>
2021-04-23 15:46:37 -0600

    Fix socials box-sizing

--- resources/views/components/socials.blade.php
+++ resources/views/components/socials.blade.php
@@ -2,18 +2,21 @@
     {{ $slot }}
 
     <div class="row justify-content-center">
-        <div class="col-2 col-lg-1 mx-4">
-            <a class="d-block" href="https://facebook.com" target="_blank">
-                <object class="mx-auto display-1" data="{{ asset('img/facebook.svg') }}" type="image/svg+xml"></object>
+        <div class="col-2 col-lg-1 mx-4 position-relative">
+            <a class="stretched-link" href="https://facebook.com" target="_blank" title="Facebook">
+                <object class="mx-auto display-1" data="{{ asset('img/facebook.svg') }}"
+                    type="image/svg+xml"></object>
             </a>
         </div>
-        <div class="col-2 col-lg-1 mx-4">
-            <a class="d-block" href="https://www.instagram.com/stoneyparkcampgrounds" target="_blank">
-                <object class="mx-auto display-1" data="{{ asset('img/instagram.svg') }}" type="image/svg+xml"></object>
+        <div class="col-2 col-lg-1 mx-4 position-relative">
+            <a class="stretched-link" href="https://www.instagram.com/stoneyparkcampgrounds" target="_blank"
+                title="Instagram">
+                <object class="mx-auto display-1" data="{{ asset('img/instagram.svg') }}"
+                    type="image/svg+xml"></object>
             </a>
         </div>
-        <div class="col-2 col-lg-1 mx-4">
-            <a class="d-block" href="https://tiktok.com" target="_blank">
+        <div class="col-2 col-lg-1 mx-4 position-relative">
+            <a class="stretched-link" href="https://tiktok.com" target="_blank" title="TikTok">
                 <object class="mx-auto display-1" data="{{ asset('img/tiktok.svg') }}" type="image/svg+xml"></object>
             </a>
         </div>
