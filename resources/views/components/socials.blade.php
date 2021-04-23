0ecb95532072d8430f3bc9a1c5395cfcdf41b880
izarooni <coalasonly@gmail.com>
2021-04-23 15:33:27 -0600

    Add instagram socials hyperlink

--- resources/views/components/socials.blade.php
+++ resources/views/components/socials.blade.php
@@ -8,7 +8,7 @@
             </a>
         </div>
         <div class="col-2 col-lg-1 mx-4">
-            <a class="d-block" href="https://instagram.com" target="_blank">
+            <a class="d-block" href="https://www.instagram.com/stoneyparkcampgrounds" target="_blank">
                 <object class="mx-auto display-1" data="{{ asset('img/instagram.svg') }}" type="image/svg+xml"></object>
             </a>
         </div>
