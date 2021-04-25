718fd48fd3b56ef0be367a090955fc2a9a5b7709
izarooni <coalasonly@gmail.com>
2021-04-24 22:37:35 -0600

    Home page styling

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -5,15 +5,15 @@
     <example-component></example-component>
     <div class="container marketing">
         <x-socials>
-            <h2 class="text-center display-4 p-5">Find us on social media</h2>
+            <h2 class="text-center display-4">Find us on social media</h2>
         </x-socials>
 
         <div class="row">
             <hr class="featurette-divider" />
             <div class="col-md-7">
-                <h2 class="featurette-heading">The view? <span class="text-muted">We got that.</span></h2>
-                <p class="lead">See beautiful mountains, tall trees and acres of greenery.</p>
-                <p><a class="btn btn-lg btn-primary" href="{{ route('gallery') }}">Browse gallery</a></p>
+                <h2 class="featurette-heading">COVID-19 Restrictions</span></h2>
+                <p class="lead">We ask taht you book only with family members or close contacts living in the same home
+                    as you.</p>
             </div>
             <div class="col-md-5">
                 <img class="h-100 img-fluid mx-auto" src="{{ asset('img/market1.jpg') }}" />
@@ -23,7 +23,7 @@
         <div class="row">
             <hr class="featurette-divider" />
             <div class="col-md-7 order-md-2">
-                <h2 class="featurette-heading">Not convinced? <span class="text-muted">See for yourself.</span></h2>
+                <h2 class="featurette-heading">The view? <span class="text-muted">See for yourself.</span></h2>
                 <p class="lead">Reservations are just a click away. Simple and fast.</p>
                 <p><a class="btn btn-lg btn-primary" href="{{ route('reserve') }}">Reserve</a></p>
             </div>
