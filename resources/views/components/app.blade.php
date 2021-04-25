5419dc9fcc2a24ad42f4e931df1b2b332e35ca70
izarooni <coalasonly@gmail.com>
2021-04-25 00:46:39 -0600

    Add OpenGraph image

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -5,8 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <meta name="og:title" content="{{ config('app.name') }} &middot; @yield('title', 'Welcome!')">
-    <meta name="og:description" content="Stoney Campgrounds">
+    <meta property="og:image" content="{{ asset('img/favicon.png') }}" />
+    <meta property="og:title" content="{{ config('app.name') }} &middot; @yield('title', 'Welcome!')">
+    <meta property="og:description" content="Stoney Park Campgrounds">
 
     <title>{{ config('app.name') }} &middot; @yield('title', 'Welcome!')</title>
 
