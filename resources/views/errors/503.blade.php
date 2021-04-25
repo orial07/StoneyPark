719d2411764d1123c527e6022a87986f151fc2c5
izarooni <coalasonly@gmail.com>
2021-04-25 04:22:29 -0600

    Add emoji to 503 error page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -2,17 +2,15 @@
 <title>Site Maintenance</title>
 <style>
     body {
+        font: 20px Helvetica, sans-serif;
+        color: #fff;
+        background: #2e2e2e;
         text-align: center;
         padding: 150px;
     }
 
     h1 {
         font-size: 50px;
-    }
-
-    body {
-        font: 20px Helvetica, sans-serif;
-        color: #333;
     }
 
     article {
@@ -32,13 +30,19 @@
         text-decoration: none;
     }
 
+    object {
+        width: 1em;
+        vertical-align: middle;
+    }
+
 </style>
 
 <article>
-    <h1>We&rsquo;ll be back soon!</h1>
+    <h1><object class="mx-auto display-1" data="{{ asset('img/sad.svg') }}"></object> We&rsquo;ll be back soon!</h1>
     <div>
         <p>Sorry for the inconvenience but the site is under maintenance at the moment. If you need to, you can always
             <a href="mailto:{{ env('MAIL_TO_ADDRESS') }}">contact us</a>, otherwise we&rsquo;ll be back online
-            shortly!</p>
+            shortly!
+        </p>
     </div>
 </article>
