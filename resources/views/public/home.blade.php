6f411c24d731cb9889d6c252d63de06643c3ca15
izarooni <coalasonly@gmail.com>
2021-05-07 23:17:52 -0600

    Home page text changes

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -5,7 +5,7 @@
         </video>
         <div class="descriptor w-100 text-center">
             <h1 class="ff-serif display-1 fw-bold">{{ config('app.name') }}</h1>
-            <p>Open From May 2021 until September 2021 via reservation</p>
+            <p>Open from May 2021 until September 2021 via reservation</p>
             <a href="{{ route('reserve') }}" class="btn btn-primary">Reserve Now!</a>
         </div>
     </div>
