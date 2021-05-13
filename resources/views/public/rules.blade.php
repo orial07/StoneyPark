3955438630f9f32a29142f5c2b93b0c2fbaa8f40
izarooni <coalasonly@gmail.com>
2021-05-13 17:43:08 -0600

    Add COVID notice to Rules page

--- resources/views/public/rules.blade.php
+++ resources/views/public/rules.blade.php
@@ -2,13 +2,15 @@
     @section('title', 'Rules')
 
     <x-container>
-        <div class="list-group">
-            @foreach ($rules as $rule)
-                <div class="mt-3">
-                    <h5 class="fs-4 mb-1 fw-bold">{!! $rule->title !!}</h5>
-                    <p>{!! $rule->description !!}</p>
-                </div>
-            @endforeach
+        <p class="lead my-5">
+            Due to current COVID-19 restrictions, spots are limited to a max of 5 people and cohorts from 2 households. We will be following <a href="https://www.alberta.ca/enhanced-public-health-measures.aspx">AHS guidelines</a> and enforcing social distancing amongst groups. Failing to comply may lead into removal of group with no refunds.
+        </p>
+
+        @foreach ($rules as $rule)
+        <div class="my-4">
+            <h3 class="mb-1 fw-bold">{!! $rule->title !!}</h3>
+            <p>{!! $rule->description !!}</p>
         </div>
+        @endforeach
     </x-container>
 </x-app>
