4be5390f6730cb54aa381a6f7916859a9524bf63
izarooni <coalasonly@gmail.com>
2021-05-13 20:09:41 -0600

    Fix emoji not showing on error pageS

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -3,7 +3,7 @@
         <div class="row justify-content-center">
             <div class="col-6">
                 <div class="d-flex align-items-center">
-                    <img class="m-5" style="height: 8rem;" src="{{ asset('img/emoji/smiling.svg') }}" />
+                    <img class="m-5" style="height: 8rem;" src="{{ asset('img/svg/smiling.svg') }}" />
                     <h1>Everything's Fine!</h1>
                 </div>
                 <p class="lead">Sorry for the inconvenience but the site is under maintenance at the moment. If you need to, you can always
